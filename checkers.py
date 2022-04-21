1c6a1010679b49235092e7821fe4eb1baaf89e52
omer lev <omer.h.lev@gmail.com>
2022-04-21 11:25:08 +0300

    added initiation of the starting pieces on the checkers board

--- checkers.py
+++ checkers.py
@@ -15,15 +15,20 @@ class checkers(game):
                 constants.CHECKERS_BOARD_LENGTH,
                 constants.CHECKERS_BOARD_WIDTH)
         self.current_player_index = 0
+        self.add_starting_pieces(self.players[0])
+        self.add_starting_pieces(self.players[1])
+        self.game_board.print_board_state()
 
     
     def add_starting_pieces(self,player):
         for i in range(constants.CHECKERS_STARTING_ROWS_NUM):
             for j in range((i%2),constants.CHECKERS_BOARD_WIDTH,2):
                 if player.name == 'Black':
-                    pass
+                    self.game_board.insert(abs(j-(constants.CHECKERS_BOARD_WIDTH-1)),
+                    abs(i-(constants.CHECKERS_BOARD_LENGTH-1))
+                    ,player.piece_type)
                 else:
-                    self.game_board.insert(i,j,player.piece_type)
+                    self.game_board.insert(j,i,player.piece_type)
 
                 
 
